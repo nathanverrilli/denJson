4a539974cd246bcfaa349c013359b240837b3ee1
Nathan Verrilli <nathan.verrilli@gmail.com>
2025-11-10 12:51:49 -0800

    add tax for cdr

--- cdrJson.go
+++ cdrJson.go
@@ -18,7 +18,7 @@ type Cdr struct {
 	StopDateTime     time.Time        `json:"stop_date_time"`
 	AuthID           string           `json:"auth_id"`
 	AuthMethod       string           `json:"auth_method"`
-	AuthorizationID  string           `json:"authorization_id"` // not showing up?
+	AuthorizationID  string           `json:"authorization_id"`
 	Location         Location         `json:"location"`
 	Tariffs          []Tariff         `json:"tariffs"`
 	ChargingPeriods  []ChargingPeriod `json:"charging_periods"`
@@ -27,13 +27,17 @@ type Cdr struct {
 	LastUpdated      time.Time        `json:"last_updated"`
 	TotalTime        decimal.Decimal  `json:"total_time"`
 	TotalParkingTime decimal.Decimal  `json:"total_parking_time"`
-	Taxes            []Tax            `json:"taxes"`
+	Taxes            []CdrTax         `json:"taxes"`
 	TotalTax         decimal.Decimal  `json:"total_tax"`
 	AveragePower     decimal.Decimal  `json:"average_power"`
 	FundingSource    FundingSource    `json:"funding_source"`
 	MaxPower         decimal.Decimal  `json:"max_power"`
 }
 
+type CdrTax struct {
+	Description string          `json:"description"`
+	Amount      decimal.Decimal `json:"amount"`
+}
 type FundingSource struct {
 	SourceType string `json:"source_type"`
 }
